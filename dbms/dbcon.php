79d560cbf55528ff6c83bfbceaa91bbeebf57aa2
Sheshnath <58323559+Shesh3799@users.noreply.github.com>
2020-10-18 22:43:01 +0530

    Added db connection file

---
+++ dbms/dbcon.php
@@ -0,0 +1,5 @@
+<?php
+	$con=mysqli_connect('localhost','username','Password','dbms');
+	//replace username with your database username most of the time its root
+	//Password is your localhost db password by defualt it comes with no password
+?>
